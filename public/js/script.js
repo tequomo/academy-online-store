41c3fdb0170bbfde6fd6e9017fe0388e55433345
Alexander Pomeshchenko <tequomo@gmail.com>
2023-03-16 02:22:37 +0300

    Fix broken rSlider

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,14 @@
-var mySlider = new rSlider({
-  target: '#sampleSlider',
-  values: [10000, 1000000],
-  range: true,
-  tooltip: true,
-  scale: true,
-  labels: false,
-  step: 10000
-});
+(function() {
+  var init = function() {
+    var mySlider = new rSlider({
+      target: '#sampleSlider',
+      values: [10000, 1000000],
+      range: true,
+      tooltip: true,
+      scale: true,
+      labels: false,
+      step: 10000
+    });
+  };
+  window.onload = init;
+})();
